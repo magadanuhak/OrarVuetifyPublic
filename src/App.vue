4f2b45ad0b3eda72bf648efe01c63f0e6b097b17
vica <cazacu@gmail.com>
2019-09-30 09:34:42 +0300

    cm2

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,131 @@
 <template>
-  <v-app>
-    <v-app-bar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
+  <v-app id="app">
+    <v-app-bar
+      app
+      clipped-left
+      color="amber"
+    >
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <span class="title ml-3 mr-5">Google&nbsp;<span class="font-weight-light">Keep</span></span>
+      <v-text-field
+        solo-inverted
+        flat
+        hide-details
+        label="Search"
+        prepend-inner-icon="search"
+      ></v-text-field>
+      <div class="flex-grow-1"></div>
     </v-app-bar>
 
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+      clipped
+      color="grey lighten-4"
+    >
+      <v-list
+        dense
+        class="grey lighten-4"
+      >
+        <template v-for="(item, i) in items">
+          <v-row
+            v-if="item.heading"
+            :key="i"
+            align="center"
+          >
+            <v-col cols="6">
+              <v-subheader v-if="item.heading">
+                {{ item.heading }}
+              </v-subheader>
+            </v-col>
+            <v-col
+              cols="6"
+              class="text-right"
+            >
+              <v-btn
+                small
+                text
+              >edit</v-btn>
+            </v-col>
+          </v-row>
+          <v-divider
+            v-else-if="item.divider"
+            :key="i"
+            dark
+            class="my-4"
+          ></v-divider>
+          <v-list-item
+            v-else
+            :key="i"
+            @click=""
+          >
+            <v-list-item-action>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title class="grey--text">
+                {{ item.text }}
+              </v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </template>
+      </v-list>
+    </v-navigation-drawer>
+
     <v-content>
-      <HelloWorld/>
+      <v-container
+        fluid
+        class="grey lighten-4 fill-height"
+      >
+        <v-row
+          justify="center"
+          align="center"
+        >
+          <Schedule>
+          </Schedule>
+        </v-row>
+      </v-container>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+  import Schedule from './components/Schedule';
+  export default {
+    props: {
+      source: String,
+    },
+    components: {
+      Schedule,
+    }
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld,
-  },
-  data: () => ({
-    //
-  }),
-};
+    ,
+    data: () => ({
+      drawer: null,
+      items: [
+        { icon: 'lightbulb_outline', text: 'Notes' },
+        { icon: 'touch_app', text: 'Reminders' },
+        { divider: true },
+        { heading: 'Labels' },
+        { icon: 'add', text: 'Create new label' },
+        { divider: true },
+        { icon: 'archive', text: 'Archive' },
+        { icon: 'delete', text: 'Trash' },
+        { divider: true },
+        { icon: 'settings', text: 'Settings' },
+        { icon: 'chat_bubble', text: 'Trash' },
+        { icon: 'help', text: 'Help' },
+        { icon: 'phonelink', text: 'App downloads' },
+        { icon: 'keyboard', text: 'Keyboard shortcuts' },
+      ],
+    }),
+  }
 </script>
+
+<style>
+#keep .v-navigation-drawer__border {
+  display: none
+}
+</style>
+
+
---
+++ src/components/Schedule.vue
@@ -0,0 +1,107 @@
+<template>
+  <v-container>
+    <v-simple-table  dark>
+      <template v-slot:default>
+        <thead>
+          <tr>
+            <th class="text-left">Lecții</th>
+            
+          </tr>
+        </thead>
+        
+        <tbody>
+          <v-container class=" ">
+    
+          <v-row v-for="lesson in schedule" 
+              :key="lesson.name"
+              justify="center" 
+              no-gutters
+              >
+            
+            <v-col v-for="subgroup in lesson.subgroup"
+                   >
+               <v-card    
+                    outlined
+                              
+                >
+      <div class=" centered-text lesson-name text-field" >{{subgroup.lesson}}</div>
+      <v-card-text class="pb-0 mb-0 classroom text-field"><span v-if="subgroup.lesson"> Auditoriu : {{subgroup.auditorium}}<span></v-card-text>
+      <v-card-text class="group-name text-field">{{subgroup.name}}</v-card-text>
+        
+    </v-card>
+            </v-col>
+           
+          </v-row>
+            </v-container>
+        </tbody>
+      </template>
+    </v-simple-table>
+  </v-container>
+</template>
+<style scoped>
+  .centered-text{
+    text-align:center!important;
+  }
+  .lesson-name{
+    font-size:16px;
+    margin-top:2px;
+    
+    font-weight:800;
+  }
+  .group-name{
+    font-size:16px;
+  }
+  .classroom{
+    
+  }
+  .text-field{
+    padding:2px;
+    margin-left:8px;
+    text-align:center;
+  }
+  .no-lesson{
+    outline-color: red!important;
+  }
+</style>
+
+<script>
+export default {
+  data () {
+    return {
+      schedule: [
+        {
+          subgroup:[
+            {
+              name : "Subgrupa 1",
+              auditorium: "101",
+              lesson : "Programare Web"
+            },
+            {
+              name : "Subgrupa 1",
+              auditorium: "101",
+              lesson : "Programare Web"
+            },
+          ]
+        },
+        {
+          subgroup:[
+            {
+              name : "Subgrupa 1",
+              auditorium: "101",
+              lesson : "Programare Web"
+            }
+            
+          ]
+        },
+        {
+          subgrup:[
+            {}
+            
+          ]
+        },
+       
+      ]
+    }
+  }
+};
+</script>
